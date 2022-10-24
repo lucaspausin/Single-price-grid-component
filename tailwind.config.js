fdecd85a1d48c620674d8139184dcaf9d456781f
lucaspausin <lucaspausin@gmail.com>
2022-10-24 20:44:35 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./index.html"],
+	theme: {
+		extend: {
+			colors: {
+				background: "#EDE4E0",
+				textAlt: "#C8DBBE",
+				textAltInv: "#bdccdb",
+				heads: "#9F8772",
+				paraGraph: "#665A48",
+				cardOne: "#d5853f",
+				cardTwo: "#e7c2a2",
+			},
+			fontFamily: {
+				Karla: "Karla",
+			},
+		},
+	},
+	plugins: [],
+};
